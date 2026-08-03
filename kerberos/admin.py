2f12431db4f61c24ddc837a2191d655d636703d3
Matheus N <matheusnunesdemoraes432@gmail.com>
2026-08-03 21:09:05 +0000

    estrutura de pastas

--- kerberos/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import (Endereco, Pet, Agendamento, Servico, Usuario)
-
-admin.site.register(Endereco)
-admin.site.register(Pet)
-admin.site.register(Agendamento)
-admin.site.register(Servico)
-admin.site.register(Usuario)
