e6dc939f13c314a1ed6fb5f3b9a2d65a4071bf6c
Matheus N <matheusnunesdemoraes432@gmail.com>
2026-08-03 21:42:08 +0000

    caminhos e view

--- ProjetoFamaPet/kerberos/urls.py
+++ ProjetoFamaPet/kerberos/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.home , name='home'),
+    path('kerberos/', views.home , name='home'),
     path('kerberos/', views.listar_usuarios , name='listar_usuarios'),
 ]
