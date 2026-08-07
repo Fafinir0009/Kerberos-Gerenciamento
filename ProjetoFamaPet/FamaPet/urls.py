ab724fe1fba8d60d8e153103bcb17184fada1cbf
Matheus N <matheusnunesdemoraes432@gmail.com>
2026-08-07 18:57:09 +0000

    ajuste na pagina clientes e agendamentos, e remoção de arquivos

--- ProjetoFamaPet/FamaPet/urls.py
+++ ProjetoFamaPet/FamaPet/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from kerberos import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home , name='home'),
     path('kerberos/', include('kerberos.urls')),
 ]
